7f62b8a9213cae5170ea8595b910da07c97ace11
Mos33001 <43756312+Mos33001@users.noreply.github.com>
2018-12-07 09:31:06 -0500

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,31 @@
+
+q1 = input("Are you human?: ")
+print("      ")
+
+print("Hello, Human")
+print("      ")
+	# add if then statement if yes then continue if not put neither am I 
+name = input("Input Your Human Name : ")
+print("      ")
+ # if numbers invalid if letters continue
+print("Hello," + " " + name)
+print("      ")
+
+age = input(name + ", " + "Please enter your human age?")
+print("      ")
+# if letters -> invalid
+print(age + ", " + "that appears to be old")
+print("     ")
+# make a greater than or less than statement where 
+# if older than say 45 that apears to be old pops up other wise u are youthful pops up
+How = input("How are you today?")
+print("    ")
+
+print(How)
+print("      ")
+print("Hmmm... ") 
+print("           ")
+
+print("That is a very human response" + "   ")
+print("      ")
+
